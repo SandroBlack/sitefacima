47f919aed30bd1e3dd9e9b3ae4c1fca7c325d00f
Elissandro <sandro-95@hotmail.com>
2018-03-14 01:29:48 -0300

    Primeiro Commit
    Att.Elissandro

---
+++ js/funcoes.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+//   	$("#btnLogin").click(function(){
+// 		 //location.href="../pages/aluno.html";
+// 		 alert("teste!");		 
+//   	}); 	
+// });
+
+document.getElementById("btnLogin").onclick = function(){
+ 	location.href="../pages/aluno.html";
+};
