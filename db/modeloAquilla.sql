9936ce8bd5d1b9a5e832b78cd819a345a0c9a6a1
Aquilla Silva Leite <aquilla11@hotmail.com>
2018-04-20 13:32:43 -0300

    att
    att

--- db/modeloAquilla.sql
+++ db/modeloAquilla.sql
@@ -1,29 +1,40 @@
 CREATE TABLE usuario(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id_usuario INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
 funcao VARCHAR(50) NOT NULL,
 nivel_acesso INT NOT NULL
 )DEFAULT CHARSET=utf8;
 
+INSERT INTO `usuario`(`id_usuario`, `nome`, `funcao`, `nivel_acesso`) VALUES (1,'Aquilla Silva Leite','Professor',1);
+
 CREATE TABLE equipamento(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id_equipamento INT PRIMARY KEY AUTO_INCREMENT,
 nome VARCHAR(30) NOT NULL,
 fabricante VARCHAR(50) NOT NULL,
 quantidade INT NOT NULL,
 estoque INT NOT NULL
 )DEFAULT CHARSET=utf8;
 
+INSERT INTO `equipamento`(`id_equipamento`, `nome`, `fabricante`, `quantidade`, `estoque`) VALUES (1,'Projetor','Epson',5,5);
+
 CREATE TABLE reservar(
-id INT PRIMARY KEY AUTO_INCREMENT,
+id_reservar INT PRIMARY KEY AUTO_INCREMENT,
+data_reserva VARCHAR(10) NOT NULL,
 periodo INT NOT NULL,
 curso VARCHAR(50) NOT NULL,
 sala VARCHAR(50) NOT NULL,
-id_usuario INT NOT NULL,
-id_equipamento INT NOT NULL
+fk_usuario INT NOT NULL,
+fk_equipamento INT NOT NULL,
+CONSTRAINT `fk_id_usuario` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`),
+CONSTRAINT `fk_id_equipamento` FOREIGN KEY (`fk_equipamento`) REFERENCES `equipamento` (`id_equipamento`)
 )DEFAULT CHARSET=utf8;
 
-ALTER TABLE reservar ADD CONSTRAINT FK_id_usuario
-FOREIGN KEY(id_usuario) REFERENCES usuario(id);
+INSERT INTO `reservar`(`id_reservar`, `data_reserva`, `periodo`, `curso`, `sala`, `fk_usuario`, `fk_equipamento`) VALUES (0,'20/04/2018',7,'Ciência Da Computação',9,1,1);  
 
-ALTER TABLE reservar ADD CONSTRAINT FK_id_equipamento
-FOREIGN KEY(id_equipamento) REFERENCES equipamento(id);
+/*
+ALTER TABLE `reservar`
+  ADD CONSTRAINT `fk_id_usuario` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`);
+  
+ALTER TABLE `reservar`
+  ADD CONSTRAINT `fk_id_equipamento` FOREIGN KEY (`fk_equipamento`) REFERENCES `equipamento` (`id_equipamento`); 
+ */
