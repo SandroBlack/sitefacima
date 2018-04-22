afd5f4c00b750f6615aea9edce42965d71cd8da3
Aquilla Silva Leite <aquilla11@hotmail.com>
2018-04-22 12:45:01 -0300

    att
    att

--- db/modeloAquilla.sql
+++ db/modeloAquilla.sql
@@ -1,40 +1,32 @@
 CREATE TABLE usuario(
 id_usuario INT PRIMARY KEY AUTO_INCREMENT,
-nome_usuario VARCHAR(30) NOT NULL,
-email_usuario VARCHAR(30) NOT NULL,
-senha_usuario VARCHAR(30) NOT NULL,
+nome_usuario VARCHAR(255) NOT NULL,
+email_usuario VARCHAR(255) NOT NULL,
+senha_usuario VARCHAR(255) NOT NULL,
 cargo_usuario VARCHAR(50) NOT NULL,
 nivel_acesso INT NOT NULL
 );
 
-INSERT INTO `usuario`(`id_usuario`, `nome_usuario`, `email_usuario`, `senha_usuario`, `cargo_usuario`, `nivel_acesso`) VALUES (1,'Aquilla Silva Leite', 'aquilla11@hotmail.com', '123', 'Professor',1);
-
 CREATE TABLE equipamento(
 id_equipamento INT PRIMARY KEY AUTO_INCREMENT,
-nome_equipamento VARCHAR(30) NOT NULL,
+nome_equipamento VARCHAR(255) NOT NULL,
 fabricante_equipamento VARCHAR(50) NOT NULL,
 quantidade_equipamento INT NOT NULL,
 patrimonio_equipamento INT NOT NULL
 );
 
-INSERT INTO `equipamento`(`id_equipamento`, `nome_equipamento`, `fabricante_equipamento`, `quantidade_equipamento`, `patrimonio_equipamento`) VALUES (1,'Projetor','Epson',5,2959);
-INSERT INTO `equipamento`(`id_equipamento`, `nome_equipamento`, `fabricante_equipamento`, `quantidade_equipamento`, `patrimonio_equipamento`) VALUES (2,'Switch','Cisco',5,2960);
-INSERT INTO `equipamento`(`id_equipamento`, `nome_equipamento`, `fabricante_equipamento`, `quantidade_equipamento`, `patrimonio_equipamento`) VALUES (3,'Roteador','D-link',5,2961);
-
 CREATE TABLE reservar(
 id_reservar INT PRIMARY KEY AUTO_INCREMENT,
-data_reserva VARCHAR(30) NOT NULL,
-hora_inicio VARCHAR(30) NOT NULL,
-hora_fim VARCHAR(30) NOT NULL,
-semestre VARCHAR(30) NOT NULL,
-curso VARCHAR(30) NOT NULL,
-sala VARCHAR(30) NOT NULL,
-periodo VARCHAR(30) NOT NULL,
+data_reserva VARCHAR(255) NOT NULL,
+hora_inicio VARCHAR(255) NOT NULL,
+hora_fim VARCHAR(255) NOT NULL,
+semestre VARCHAR(255) NOT NULL,
+curso VARCHAR(255) NOT NULL,
+sala VARCHAR(255) NOT NULL,
+periodo VARCHAR(255) NOT NULL,
 fk_usuario INT NOT NULL,
 fk_equipamento INT NOT NULL,
 CONSTRAINT `fk_id_usuario` FOREIGN KEY (`fk_usuario`) REFERENCES `usuario` (`id_usuario`),
 CONSTRAINT `fk_id_equipamento` FOREIGN KEY (`fk_equipamento`) REFERENCES `equipamento` (`id_equipamento`)
 );
-
-INSERT INTO `reservar`(`id_reservar`, `data_reserva`, `hora_inicio`, `hora_fim`, `semestre`, `curso`, `sala`, `periodo`, `fk_usuario`, `fk_equipamento`) VALUES (1,'23/04/2018', '1° Aula','2° Aula',7,'Ciência Da Computação',9,'Noturno',1,1);
 
